de05fcb863d648599f71e95aed3928a088d2dbdd
Nellie <fabybacenelson@gmail.com>
2019-12-03 21:31:12 -0700

    added buttons, notes functions, and event listeners

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     let currDate = new Date();
 
@@ -6,40 +6,52 @@ $(document).ready(function(){
     console.log(hour);
 
     // generate table to show current day's schedule
-    function genTable(){
+    // style past/current/future hours
+    function genTable() {
 
         let hourCount = (hour - 4) % 24;
 
-        for(let i = 0; i < 12; i++){
-            let tr = $("<tr></tr>").attr({ "id" : i});
+        for (let i = 0; i < 12; i++) {
+            let tr = $("<tr></tr>").attr({
+                "id": i
+            });
             $("#planner").append(tr);
-
-            
 
             let timeTag = formatTime(hourCount);
 
-            let td = $("<td></td>").text(timeTag);
-            tr.append(td);
+            let timeCol = $("<td></td>").text(timeTag);
+            tr.append(timeCol);
 
             hourCount = (hourCount + 1) % 24;
-            console.log(i + " " + hourCount + " " + timeTag);
+            // console.log(i + " " + hourCount + " " + timeTag);
+
+            let noteCol = $("<td></td>").text(getNotes());
+            tr.append(noteCol);
+
+            //add button w/ event listener
+            let addCol = $("<td></td>");
+            let addButt = $("<button></button>").text("Edit Notes");
+            tr.append(addCol);
+            addCol.append(addButt);
+
+            addButt.on("click", addNotes);
 
         }
     }
 
-    
-    function formatTime(h){
+
+    function formatTime(h) {
         let formTime = h;
         let amPM = " AM";
 
-        if(h >= 12){
+        if (h >= 12) {
             formTime = h - 12;
             amPM = " PM";
-        }else if (h < 12){
+        } else if (h < 12) {
             amPM = " AM"
         }
 
-        if(formTime == 0){
+        if (formTime == 0) {
             formTime = 12;
         }
 
@@ -48,16 +60,21 @@ $(document).ready(function(){
 
     genTable();
     // populate table with events saved to local storage
-
-
+    function getNotes(){
+        return "I am placeholder text";
+    }
 
     // allow user to add events to local storage
+    function addNotes(){
+        console.log("add notes");
+        // let test = this.parent().parent().attr("id");
+        // console.log(test);
+    }
 
 
     // setTimeout() to update dynamically?
 
 
 
+
 });
-    
-    
